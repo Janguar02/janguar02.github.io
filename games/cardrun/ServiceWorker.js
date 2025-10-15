596900fdd93bbb2fdbc4a9d46faff4912a82f478
Janguar02 <131951627+Janguar02@users.noreply.github.com>
2025-10-15 18:30:39 +0200

    Added Cardrun

---
+++ games/cardrun/ServiceWorker.js
@@ -0,0 +1,33 @@
+const cacheName = "Soooodle-CardRun-1.0.4";
+const contentToCache = [
+    "Build/v8.loader.js",
+    "Build/v8.framework.js.unityweb",
+    "Build/v8.data.unityweb",
+    "Build/v8.wasm.unityweb",
+    "TemplateData/style.css"
+
+];
+
+self.addEventListener('install', function (e) {
+    console.log('[Service Worker] Install');
+    
+    e.waitUntil((async function () {
+      const cache = await caches.open(cacheName);
+      console.log('[Service Worker] Caching all: app shell and content');
+      await cache.addAll(contentToCache);
+    })());
+});
+
+self.addEventListener('fetch', function (e) {
+    e.respondWith((async function () {
+      let response = await caches.match(e.request);
+      console.log(`[Service Worker] Fetching resource: ${e.request.url}`);
+      if (response) { return response; }
+
+      response = await fetch(e.request);
+      const cache = await caches.open(cacheName);
+      console.log(`[Service Worker] Caching new resource: ${e.request.url}`);
+      cache.put(e.request, response.clone());
+      return response;
+    })());
+});
